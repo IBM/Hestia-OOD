29c0fbaec1bebe70c25d60af84a6f2f56e43adb3
Raúl Fernández Díaz <raulfd@ibm.com>
2024-03-06 14:59:06 +0000

    🛠️ Setup file for PIP publishing

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+import os
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+this_directory = Path(__file__).parent
+readme = (this_directory / "README.md").read_text()
+
+requirements = [ 
+    'networkx',
+    'scipy',
+    'scikit-learn',
+    'pandas',
+    'numpy',
+    'rdkit',
+    'tqdm'
+]
+
+test_requirements = requirements
+
+setup(
+author="Raul Fernandez-Diaz",
+    author_email='raul.fernandezdiaz@ucdconnect.ie',
+    python_requires='>=3.9',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    description="Suite of tools for analysing the independence between training and evaluation biosequence datasets and to generate new generalisation-evaluating hold-out partitions",
+    entry_points={
+    },
+    install_requires=requirements,
+    license="MIT",
+    long_description=readme + '\n\n',
+    include_package_data=True,
+    keywords='hestia',
+    name='hestia',
+    packages=find_packages(),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/IBM/Hestia',
+    version='0.1.0',
+    zip_safe=False,
+)
