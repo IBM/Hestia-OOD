497842f094069d2422ec5345b41cdcf41073b58f
RaulFD-creator <raulfdz9@gmail.com>
2024-10-30 09:08:49 +0000

    🚀 Version: Prepare version 0.0.32

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/IBM/Hestia-OOD',
-    version='0.0.31',
+    version='0.0.32',
     zip_safe=False,
 )
